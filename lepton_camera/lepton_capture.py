f0393ceb5809ca6262a793f2b5f3dda877ec432f
Charles Dickens <charlieadickens@gmail.com>
2018-08-07 13:07:10 -1000

    letpton camera
    letpton camera

---
+++ lepton_camera/lepton_capture.py
@@ -0,0 +1,17 @@
+import numpy as np
+from pylepton import Lepton
+from PIL import Image
+from PIL import BICUBIC
+
+
+def capture_image(size, filename):
+    with Lepton() as l:
+        a,_ = l.capture()
+
+    #Create a PIL image from numpy array
+    image = Image.fromarray(a.astype('uint8'), 'RGB')
+
+    #resize PIL image to match picamera resolution
+    image = image.resize(size, BICUBIC)
+
+    image.save(filename, 'JPEG')

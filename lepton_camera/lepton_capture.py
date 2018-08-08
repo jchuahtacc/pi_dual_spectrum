168e36f223a388d42a407af86291a92aeb5ddee6
Charles Dickens <charlieadickens@gmail.com>
2018-08-07 14:48:50 -1000

    import error

--- lepton_camera/lepton_capture.py
+++ lepton_camera/lepton_capture.py
@@ -1,8 +1,6 @@
 import numpy as np
 from pylepton import Lepton
 from PIL import Image
-from PIL import BICUBIC
-
 
 def capture_image(filename, size):
     with Lepton() as l:
@@ -12,6 +10,6 @@ def capture_image(filename, size):
     image = Image.fromarray(a.astype('uint8'), 'RGB')
 
     #resize PIL image to match picamera resolution
-    image = image.resize(size, BICUBIC)
+    image = image.resize(size, Image.BICUBIC)
 
     image.save(filename, 'JPEG')
